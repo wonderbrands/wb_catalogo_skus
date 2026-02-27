977fc7ab946d4d175fe654e8bdff2985f07b28ee
Sergio Gil <sergio@wonderbrands.co>
2026-02-27 17:22:09 -0600

    [UPD] Update to v18.0

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Catálogo de SKUs WB",
+    'summary': "Aplicación principal para la gestión y filtro de SKUs",
+    'description': """
+        Módulo diseñado para reemplazar gestión de productos.
+    """,
+    'author': "Sergio Guerrero",
+    'category': 'Inventory',
+    'version': '18.0.1.0',
+    'depends': ['base', 'product', 'stock'],
+    'application': True,
+    'sequence': 10,
+    'data': [
+        'views/catalogue_menus.xml',
+        'views/catalogue_search.xml',
+    ],
+}
